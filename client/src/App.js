49bd501ae0aaf7eda656a114c06ff9b6bd608bcd
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-09-14 16:40:11 +0900

    Add Get Customers REST API modle

--- client/src/App.js
+++ client/src/App.js
@@ -20,38 +20,26 @@ const styles = theme =>({
 	}
 })
 
-const customers=[
-  	{
-  	  	id:'1',
-    	image:'https://placeimg.com/64/64/any',
-    	name: '안현진',
-    	birthday: '911206',
-    	gender: 'male',
-    	job: 'programmer'
 
-	},
-  	{
-    	id:'2',
-    	image:'https://placeimg.com/64/64/2',
-    	name: '이순신',
-    	birthday: '951206',
-    	gender: 'male',
-    	job: 'student'
-  	},
-  	{
-    	id:'3',
-    	image:'https://placeimg.com/64/64/3',
-    	name: '손님',
-    	birthday: '941206',
-    	gender: 'male',
-    	job: 'student'  
+class App extends Component{	
+	state = {
+		customers: ""
 	}
-]
 
-class App extends Component{
-	
-  render(){
-	  const {classes} = this.props;
+	componentDidMount(){
+		this.callApi()
+		.then(res => this.setState({customers: res}))
+		.catch(err =>console.log(err))
+	}
+
+	callApi = async () =>{
+		const response = await fetch('/api/customers');
+		const body = await response.json();
+		return body;
+	}
+
+	render(){
+		const {classes} = this.props;
 
     return(
       <Paper className={classes.root}>
@@ -67,8 +55,8 @@ class App extends Component{
 				</TableRow>
 			</TableHead>
           <TableBody>
-          {
-          customers.map(c => {
+          {this.state.customers ?
+          this.state.customers.map(c => {
             return (
 				<Customer
 					key={c.id}
@@ -79,9 +67,10 @@ class App extends Component{
 					job={c.job}
 					gender={c.gender}
 				/>
-            )
-          })
-        }
+            );
+		  })
+		  : ""}
+	
         </TableBody>
 
 
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: "hello express"});
-})
+app.get('/api/customers',(req, res)=>{
+    res.send([
+        {
+            id:'1',
+            image:'https://placeimg.com/64/64/any',
+            name: '안현진',
+            birthday: '911206',
+            gender: 'male',
+            job: 'programmer'
+
+	    },
+  	    {
+            id:'2',
+            image:'https://placeimg.com/64/64/2',
+            name: '이순신',
+            birthday: '951206',
+            gender: 'male',
+            job: 'student'
+  	    },
+  	    {
+            id:'3',
+            image:'https://placeimg.com/64/64/3',
+            name: '손님',
+            birthday: '941206',
+            gender: 'male',
+            job: 'student'  
+	    }        
+    ]);
+});
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
