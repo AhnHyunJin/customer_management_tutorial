024b441a6d27bde85dc0566f9dce30de72335822
DESKTOP-DI0HS5K\hjan1 <hjan7804@gmail.com>
2019-09-17 14:28:57 +0900

    upload data to db from client UI(using post)

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -37,11 +37,19 @@ class CustomerAdd extends React.Component{
             .then((response) => {
                 console.log(response.data)
             });
+        this.setState({
+            file:null,
+            userName:'',
+            birthday:'',
+            gender:'',
+            job:'',
+            fileName:'',
+        })
     }
 
     handleFileChange = (e)=>{
         this.setState({
-            file: e.target.file[0],
+            file: e.target.files[0],
             fileName: e.target.value
         })
     }
